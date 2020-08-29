da4b66169c583fca9b3bbd15f19734d8d29c404a
Matija <matija.vavetic@gmail.com>
2020-08-29 14:31:53 +0200

    Reply form

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -16,7 +16,6 @@
                 </div>
             </div>
         </div>
-    </div>
 
     <div class="row">
         <div class="row justify-content-center">
@@ -26,5 +25,25 @@
                 @endforeach
             </div>
         </div>
+    </div>
+
+    @if (auth()->check())
+    <div class="row">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <textarea name="body" id="body" class="form-control" placeholder="Type body"></textarea>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Post</button>
+                </form>
+            </div>
+        </div>
+    </div>
+        @else
+            <p class="text-center">Please sign in to participate in this discussion.</p>
+        @endif
     </div>
 @endsection
