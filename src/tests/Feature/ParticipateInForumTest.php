672984aedce67f9abc48e8327bdbf49ddef8a242
Matija <matija.vavetic@gmail.com>
2020-08-30 16:13:50 +0200

    Implemented Docker as local environment

---
+++ src/tests/Feature/ParticipateInForumTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Reply;
+use App\Thread;
+use App\User;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class ParticipateInForumTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function unauth_user_may_not_add_replies()
+    {
+        $this->expectException(AuthenticationException::class);
+        $this->withoutExceptionHandling();
+
+        $this->post('/threads/1/replies', []);
+    }
+
+    /** @test */
+    public function an_auth_user_may_participate_in_forum_threads()
+    {
+        $user = create(User::class);
+        $this->be($user);
+
+        $thread = create(Thread::class);
+
+        $reply = make(Reply::class);
+
+        $this->post($thread->path().'/replies', $reply->toArray());
+
+        $this->get($thread->path())
+            ->assertSee($reply->body);
+    }
+}
---
+++ src/tests/Feature/ReadThreadsTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ReadThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    private $thread;
+
+    public function setUp() : void
+    {
+        parent::setUp();
+
+        $this->thread = create('App\Thread');
+    }
+
+    /** @test */
+    public function a_user_can_view_all_threads()
+    {
+        $response = $this->get('/threads');
+
+        $response->assertSee($this->thread->title);
+    }
+
+    /** @test */
+    public function a_user_can_read_a_single_thread()
+    {
+        $response = $this->get('/threads/' . $this->thread->id);
+
+        $response->assertSee($this->thread->title);
+    }
+
+    /** @test */
+    public function a_user_can_read_replies_that_are_associated_with_a_thread()
+    {
+        $reply = create('App\Reply', ['thread_id' => $this->thread->id]);
+
+        $response = $this->get('/threads/' . $this->thread->id);
+
+        $response->assertSee($reply->body);
+    }
+}
